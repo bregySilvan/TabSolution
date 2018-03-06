197106ddbd016065ca1303d25d867f7e97c4fe0f
Oliver Werlen <oliver.werlen@hevs.ch>
2018-03-06 12:33:38 +0100

    Get-Request for all
    This is the final code for getting all the informations on all URLs available on the server.

--- exemples/TabSolutionsClient/src/app/app.component.ts
+++ exemples/TabSolutionsClient/src/app/app.component.ts
@@ -12,6 +12,7 @@ interface ServerResponse {
 
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -27,15 +28,27 @@ export class AppComponent implements OnInit {
 
 
   ngOnInit(): void {
-  
+ 
     this.http.get<ServerResponse>('http://localhost:8888/board/1').subscribe(data => {
     console.log("Filmtitle: "+ data.title);
     console.log("Number of series: "+ data.anzahlFolgen);
     console.log("rating: "+ data.rating);
-      //console.log(data);
+      }
+    );
+   //Code for getRequest on /data
+    this.http.get<ServerResponse>('http://localhost:8888/data').subscribe(data => {
+      console.log("Filmtitle: "+ data.ID1.title);
+      console.log("Number of series: "+ data.ID1.anzahlFolgen);
+      console.log("rating: "+ data.ID1.rating);
+    }
+  );
+
+   //Code for getRequest on /title/id
+    this.http.get<ServerResponse>('http://localhost:8888/title/3').subscribe(data => {
       
-      //console.log(data.ID1); 
-  }
+      console.log("Filmtitle: "+ data.title);
+
+      }
     );
   }
 
