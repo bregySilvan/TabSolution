c8b203410e8f34de16952f318d6ec378347ff5ad
Oliver Werlen <oliver.werlen@hevs.ch>
2018-03-06 13:32:16 +0100

    #13 Get-Request working
    In this Commit, everything is working fine

--- exemples/TypeScript/greeter_simple.ts
+++
@@ -1,9 +0,0 @@
-{
-    function greet(person: string) {
-        return "Hello, " + person;
-    }
-
-    let user = "Jane User";
-
-    console.log( greet(user) );
-}
--- exemples/server/server.ts
+++ exemples/server/server.ts
@@ -1,59 +1,72 @@
 /*var express = require('express');
 var fs = require("fs");
-
 var app = express();
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.get('/data', function (req, res) {
-    fs.readFile( __dirname + "/" + "daten.json", 'utf8', function (err, data) {
-        res.end( data );
+    fs.readFile(__dirname + "/" + "daten.json", 'utf8', function (err, data) {
+        res.end(data);
     });
-})
-
+});
 app.get('/title/:id', function (req, res) {
-    let ID:string = req.params.id;
+    var ID = req.params.id;
     ID = "ID" + ID;
-    fs.readFile( __dirname + "/" + "daten.json", 'utf8', function (err, data) {
-        let daten = JSON.parse( data );
-        let title:string;
-
-        try {
-            title = daten[ID].title;
-        }
-        catch(err) {
-            title = "ERROR ID ist nicht vorhanden :("
-        }
-
-        res.end( '{"' + title + '"}' );
-    });
-})
-
-app.get('/board/:id', function (req, res) {
-    let ID:string = req.params.id;
-    ID = "ID" + ID;
-    fs.readFile( __dirname + "/" + "daten.json", 'utf8', function (err, data) {
-        let daten = JSON.parse( data );
-        let obj;
-
+    fs.readFile(__dirname + "/" + "daten.json", 'utf8', function (err, data) {
+        var daten = JSON.parse(data);
+        var obj;
         try {
             obj = {
                 title: daten[ID].title,
-                anzahlFolgen: daten[ID].anzahlFolgen, 
+            };
+        }
+        catch (err) {
+            obj = "ERROR ID ist nicht vorhanden :(";
+        }
+        var titleString = JSON.stringify(obj);
+        res.end(titleString);
+    });
+});
+app.get('/board/:id', function (req, res) {
+    var ID = req.params.id;
+    ID = "ID" + ID;
+    fs.readFile(__dirname + "/" + "daten.json", 'utf8', function (err, data) {
+        var daten = JSON.parse(data);
+        var obj;
+        try {
+            obj = {
+                title: daten[ID].title,
+                anzahlFolgen: daten[ID].anzahlFolgen,
                 rating: daten[ID].rating
-            }
+            };
         }
-        catch(err) {
-            obj = "ERROR ID ist nicht vorhanden :("
+        catch (err) {
+            obj = "ERROR ID ist nicht vorhanden :(";
         }
-        
-        let dataString:string = JSON.stringify(obj);
-
+        var dataString = JSON.stringify(obj);
         res.end(dataString);
     });
-})
-
-
+});
 var server = app.listen(8888, function () {
-    let port = server.address().port
-    console.log("Runnint express at: " + port)
-})
+    var port = server.address().port;
+    console.log("Runnint express at: " + port);
+});
 */
