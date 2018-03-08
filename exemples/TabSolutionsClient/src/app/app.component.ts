08faa7fdd5aff37a4a2e21f71d8b02e1189dad5b
Oliver Werlen <oliver.werlen@hevs.ch>
2018-03-08 08:26:38 +0100

    Showing the result of the request on the page
    You see all the informations from the server on the mainpage

--- exemples/TabSolutionsClient/src/app/app.component.ts
+++ exemples/TabSolutionsClient/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 
-interface ServerResponse {
+interface IBoard {
   title: string;
   anzahlFolgen;
   rating;
-  
-  ID1; 
+  ID2; 
 }
 
 
@@ -16,41 +15,50 @@ interface ServerResponse {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'], 
+
+   
 })
 export class AppComponent implements OnInit {
   title = 'app';
   results = '';
+  
+
+  currentBoard: IBoard = null; //new board with the attributes from the interface IBoard
+  requestURLBoard: string = 'http://localhost:8888/board/1';
+  /*requestURLData: string = 'http://localhost:8888/data';
+  requestURLTitle: string = 'http://localhost:8888/title/1';*/
+
 
   constructor(private http: HttpClient){
-
+    this.http.get('http://localhost:8888/board/1').subscribe((data:IBoard) => {
+    this.currentBoard = data;
+      }
+    );
   }
 
 
   ngOnInit(): void {
  
-    this.http.get<ServerResponse>('http://localhost:8888/board/1').subscribe(data => {
-    console.log("Filmtitle: "+ data.title);
-    console.log("Number of series: "+ data.anzahlFolgen);
-    console.log("rating: "+ data.rating);
-      }
-    );
+    
+    /*
    //Code for getRequest on /data
-    this.http.get<ServerResponse>('http://localhost:8888/data').subscribe(data => {
-      console.log("Filmtitle: "+ data.ID1.title);
-      console.log("Number of series: "+ data.ID1.anzahlFolgen);
-      console.log("rating: "+ data.ID1.rating);
+    this.http.get<IBoard>('http://localhost:8888/data').subscribe(data => {
+      console.log("Filmtitle: "+ data.ID2.title);
+      console.log("Number of series: "+ data.ID2.anzahlFolgen);
+      console.log("rating: "+ data.ID2.rating);
     }
   );
 
    //Code for getRequest on /title/id
-    this.http.get<ServerResponse>('http://localhost:8888/title/3').subscribe(data => {
+    this.http.get<IBoard>('http://localhost:8888/title/3').subscribe(data => {
       
       console.log("Filmtitle: "+ data.title);
 
       }
     );
   }
-
+*/
+  }
 }
 
