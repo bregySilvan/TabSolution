6b74b1197fd2d34fd7606dcfac159ca3f7a3257d
Oliver Werlen <oliver.werlen@hevs.ch>
2018-03-08 16:28:47 +0100

    Routing working with values from get Request

--- exemples/TabSolutionsClient/src/app/board/board.component.ts
+++ exemples/TabSolutionsClient/src/app/board/board.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+interface IBoard {
+  title: string;
+  description;
+  imageURL;
+  ID2; 
+}
 
 @Component({
   selector: 'app-board',
@@ -7,7 +15,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
 
-  constructor() { }
+  currentBoard: IBoard = null; //new board with the attributes from the interface IBoard
+  requestURLBoard: string = 'http://localhost:8888/board/2';
+  /*requestURLData: string = 'http://localhost:8888/data';
+  requestURLTitle: string = 'http://localhost:8888/title/1';*/
+
+
+  constructor(private http: HttpClient){
+    this.http.get(this.requestURLBoard).subscribe((data:IBoard) => {
+    this.currentBoard = data;
+    console.log(data.title);
+      }
+    );
+  }
 
   ngOnInit() {
   }
