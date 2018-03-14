83bde3aabbb2f694acd2f5933689e21dbb2dfdf2
Oliver Werlen <oliver.werlen@hevs.ch>
2018-03-14 12:23:48 +0100

    Add id from selectedBoards to array

--- src/TabSolutionsClient/src/app/settings/settings.component.ts
+++ src/TabSolutionsClient/src/app/settings/settings.component.ts
@@ -11,21 +11,45 @@ import { IBoardInfo } from '../../../../interfaces';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-
-  setShit():void{
-    this.hansueli = "fack da shit"; 
-    console.log("hans");
-
-    }
+  
   requestTarget: string = 'localhost';
   requestPort: number = config.defaultPort;
   requestLocation: string = '/boardlist';
   requestPayload = '2';
   requestURLBoard: string = `http://${this.requestTarget}:${this.requestPort}${this.requestLocation}`
   boardInfos: IBoardInfo[] = [];
-  hansueli:string = "nothing"; 
+  hansueli:string = ""; 
+  selectedBoards:string[] = [];
+  index: number;
 
   
+  addSelectedToArray(id:string):void
+  {
+    this.hansueli = ""; 
+    if(!this.selectedBoards.includes(id))
+    {
+      this.selectedBoards.push(id);
+      for (let i of this.selectedBoards) {
+        //console.log(i); // "4", "5", "6"
+     }
+    }
+    else
+    {
+      this.index = this.selectedBoards.indexOf(id);
+      this.selectedBoards.splice(this.index);
+      for (let i of this.selectedBoards) {
+        //console.log(i); // "4", "5", "6"
+     }
+    }
+      
+  }   
+  giveItOut():void
+    { 
+      for (let i of this.selectedBoards) {  
+        this.hansueli += i+", "; 
+     }
+    }
+
 
   constructor(private http: HttpClient) { 
 
