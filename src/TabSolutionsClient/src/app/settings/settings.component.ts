6fa01c10d320a5c7234648d54fe71495d907e092
Christian Seematter <christian.seematter@gmail.com>
2018-03-14 16:55:45 +0100

    #32. Implement simple POST for /title
    - Tested working with the current server on master
    - Log correct Angular HTTPClient error if something goes wrong in
    console

--- src/TabSolutionsClient/src/app/settings/settings.component.ts
+++ src/TabSolutionsClient/src/app/settings/settings.component.ts
@@ -16,42 +16,36 @@ import 'rxjs/add/operator/catch';
 })
 export class SettingsComponent implements OnInit {
 
-  board: IBoard = {
-    title: 'sword master',
-    description: 'my first description',
-    imageURL: 'https://www.w3schools.com/w3css/img_lights.jpg'
+  board = {
+    title: 'sword master'
   };
 
   requestHost = 'localhost';
   requestPort = '34534';
-  requestLocation = 'boards';
+  requestLocation = 'title';
   requestTargetId = '1';
   requestUrlBoard = `http://${this.requestHost}:${this.requestPort}/${this.requestLocation}/${this.requestTargetId}`;
 
   constructor(private http: HttpClient) {
-    //this.http.post(this.requestUrlBoard, this.board, httpPostOptions)
-      //.catch((error:any) => Observable.throw(error.json().error))
-    //.pipe(catchError(this.handleBoardPostError('error: 'this.error , this.board)));
 
-    this.create(this.board);
-  }
-
-  create(board: IBoard): Observable<any> {
-    return this.http
-      .post(this.requestUrlBoard, this.board, httpPostOptions)
-      .map(response => {
-        if (response.status === 200) console.log("status 200 OK");
-        return response;
-      })
-      .catch((error: any) => console.log(error));
   }
 
   ngOnInit() {
-
+    this.postTitle();
   }
 
+  postTitle() {
+    console.log('posting');
+    this.http.post(this.requestUrlBoard, this.board)
+    .subscribe(
+      data => console.log(data),
+      err => console.error(err),
+      () => console.log('yay')
+    );
+   }
+
   handleBoardPostError(err: any, sentBoard: any) {
-    console.log(err);
+    console.error(err);
     console.log(sentBoard);
     return Observable.create({});
   }
