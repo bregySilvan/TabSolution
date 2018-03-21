77967bd736022b903e9ce22fa9ea128c23b8b24c
TheScorpio13 <35956105+TheScorpio13@users.noreply.github.com>
2018-03-21 14:34:17 +0100

    title become the id from the payload and not from the url

--- src/server/app/server.ts
+++ src/server/app/server.ts
@@ -34,9 +34,8 @@ app.get('/data', function (req, res) {
     });
 });
 
-app.get('/title/:id', function (req, res) {
-    var ID = req.query.id;
-    //ID = "ID" + ID;
+app.get('/title', function (req, res) {
+    var ID = req.query;
     fs.readFile(dataFile, 'utf8', function (err, data) {
         var daten = JSON.parse(data);
         console.log(daten);
@@ -54,10 +53,9 @@ app.get('/title/:id', function (req, res) {
     });
 });
 
-app.get('/board/:id', function (req, res) {
-    var idArray = [1,2];
-    var ID = req.query.id;
-    //ID = "ID" + ID;
+app.get('/board', function (req, res) {
+    //var idArray = [1,2];
+    var idArray = req.query;
     fs.readFile(dataFile, 'utf8', function (err, data) {
         var daten = JSON.parse(data);
         var board: IBoard;
