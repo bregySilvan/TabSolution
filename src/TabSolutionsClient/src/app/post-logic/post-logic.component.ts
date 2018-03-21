e2dc6841954bc1ae2bff5614fa4aa59a5284bd3a
SvenHutter <35990700+SvenHutter@users.noreply.github.com>
2018-03-21 14:58:38 +0100

    Update post-logic from #32

--- src/TabSolutionsClient/src/app/post-logic/post-logic.component.ts
+++ src/TabSolutionsClient/src/app/post-logic/post-logic.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { IBoard } from '../../../../interfaces';
+import { httpPostOptions } from '../../../../config';
+import { catchError, retry } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
-  selector: 'app-post-logic',
-  templateUrl: './post-logic.component.html',
-  styleUrls: ['./post-logic.component.css']
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
 })
-export class PostLogicComponent implements OnInit {
+export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  board = {
+    title: 'sword master'
+  };
+
+  requestHost = 'localhost';
+  requestPort = '34534';
+  requestLocation = 'title';
+  requestTargetId = '1';
+  requestUrlBoard = `http://${this.requestHost}:${this.requestPort}/${this.requestLocation}/${this.requestTargetId}`;
+  titletest = "";
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  myform: FormGroup;
+  title: FormControl;
+
 
   ngOnInit() {
+
+  }
+
+  onSubmit() { 
+    this.postBoard();
+  };
+
+  postBoard() {
+    console.log('posting');
+    this.http.post(this.requestUrlBoard, this.board)
+    .subscribe(
+      data => console.log(data),
+      err => console.error(err),
+      () => console.log('yay')
+    );
+     this.titletest = this.board.title;
+   }
+
+  handleBoardPostError(err: any, sentBoard: any) {
+    console.error(err);
+    console.log(sentBoard);
+    return Observable.create({});
   }
 
 }
