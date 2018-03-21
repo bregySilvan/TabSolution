08b378050d80dfedb38a9bc3f45840a2339aaf80
Oliver Werlen <oliver.werlen@hevs.ch>
2018-03-21 14:54:24 +0100

    Added new component for the postFormular
    The Post formular is included in the Settings-Component

---
+++ src/TabSolutionsClient/src/app/post-logic/post-logic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-logic',
+  templateUrl: './post-logic.component.html',
+  styleUrls: ['./post-logic.component.css']
+})
+export class PostLogicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
