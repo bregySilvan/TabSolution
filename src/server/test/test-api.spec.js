a52e6eabdcd8fd2009605feb1c2a0b258162e3c3
bregySilvan <31403441+bregySilvan@users.noreply.github.com>
2018-03-28 16:06:16 +0200

    #travis setup first test
    - Travis should now run the first ever created test for tabsolution

---
+++ src/server/test/test-api.spec.js
@@ -0,0 +1,9 @@
+
+describe('testing the test framework', () => {
+    it('testing strign equatility', () => {
+        expect('resultText').toBe('resultText');
+    });
+    it('testing strign equatility', () => {
+        expect('notResultText').toBe('resultText');
+    });
+});
